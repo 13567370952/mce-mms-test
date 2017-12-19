498cfcaea15062f1ba99f3359348392ba391c912
yshenhn <yang.shen@anchora.me>
2017-12-19 17:33:28 +0800

    aa

--- compute-service/src/main/java/com/didispace/ComputeServiceApplication.java
+++ compute-service/src/main/java/com/didispace/ComputeServiceApplication.java
@@ -2,11 +2,17 @@ package com.didispace;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ComputeServiceApplication {
+public class ComputeServiceApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ComputeServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
